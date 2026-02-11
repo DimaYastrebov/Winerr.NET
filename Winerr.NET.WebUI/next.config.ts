e35fa299605fc0a9e37a0890e7fc9a6b43660ce6
DimaYastrebov <stepanenko.dimon.10@gmail.com>
2026-02-11 04:15:40 +0200

    Update next.config.ts

--- Winerr.NET.WebUI/next.config.ts
+++ Winerr.NET.WebUI/next.config.ts
@@ -1,14 +1,24 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/v1/:path*",
-        destination: "http://localhost:5000/v1/:path*",
-      },
-    ];
+  output: isProd ? 'export' : undefined,
+  
+  images: {
+    unoptimized: true,
   },
+
+  ...(!isProd ? {
+    async rewrites() {
+      return [
+        {
+          source: "/v1/:path*",
+          destination: "http://localhost:5000/v1/:path*",
+        },
+      ];
+    },
+  } : {}),
 };
 
 export default nextConfig;
